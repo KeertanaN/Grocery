bb94d4ffb74feb703c188c1e152819d6fe76b35b
Keertana Nandyala (HCL America Inc) <v-kenand@microsoft.com>
2019-08-30 01:00:35 -0700

    Making changes to html

--- src/app/app.grocery.ts
+++ src/app/app.grocery.ts
@@ -1,20 +1,19 @@
 import { Component } from '@angular/core';
-
+ 
 @Component({
     selector: 'app-grocery',
     templateUrl: './app.grocery.html',
     styleUrls: [
         '../assets/css/bootstrap.min.css'
     ]
-  })
-  export class GroceryComponent {
-        task: string;
-        tasks: [];
-        // onClick(){
-        //     this.tasks.push({name: this.task});
-        //     this.task='';
-        // }
-
-  }
-  
+})
  
+export class GroceryComponent {
+  task: string;
+  tasks = [];
+ 
+  onClick(){
+    this.tasks.push({name: this.task});
+    this.task = '';
+  }
+}
